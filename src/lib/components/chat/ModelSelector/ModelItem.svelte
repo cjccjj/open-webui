66ad7333daa88a5b2866fd43c91329d59156a6d3
CJ_homepc <changjun.z@gmail.com>
2025-06-14 08:27:37 +0000

    hide the model tag lable

--- src/lib/components/chat/ModelSelector/ModelItem.svelte
+++ src/lib/components/chat/ModelSelector/ModelItem.svelte
@@ -210,6 +210,7 @@
 				{/if}
 			</div>
 
+			<!--
 			{#if (item?.model?.tags ?? []).length > 0}
 				<div
 					class="flex gap-0.5 self-center items-start h-full w-full translate-y-[0.5px] overflow-x-auto scrollbar-none"
@@ -225,6 +226,7 @@
 					{/each}
 				</div>
 			{/if}
+			-->
 
 		</div>
 	</div>
