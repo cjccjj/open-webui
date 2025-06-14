c825f9a7e2cd084c550d823094e021847084c9f5
CJ_homepc <changjun.z@gmail.com>
2025-06-14 16:20:52 +0000

    remove ctrl-r for real

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1202,14 +1202,14 @@
 
 													// Check if Ctrl + R is pressed
 													if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
-														e.preventDefault();
-														console.log('regenerate');
+														// e.preventDefault();
+														console.log('regenerate skipped');
 
 															const regenerateButton = [
 																...document.getElementsByClassName('regenerate-response-button')
 															]?.at(-1);
 
-														regenerateButton?.click();
+														// regenerateButton?.click();
 													}
 
 														if (prompt === '' && e.key == 'ArrowUp') {
@@ -1423,14 +1423,14 @@
 
 												// Check if Ctrl + R is pressed
 												if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
-													e.preventDefault();
-													console.log('regenerate');
+													// e.preventDefault();
+													console.log('regenerate skipped');
 
 													const regenerateButton = [
 														...document.getElementsByClassName('regenerate-response-button')
 													]?.at(-1);
 
-													regenerateButton?.click();
+													// regenerateButton?.click();
 												}
 
 												if (prompt === '' && e.key == 'ArrowUp') {
