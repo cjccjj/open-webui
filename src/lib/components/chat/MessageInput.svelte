0ece8f1228a183f1daeb94ba89d3bcef845dfa30
CJ_homepc <changjun.z@gmail.com>
2025-06-14 16:19:30 +0000

    Revert "removed shortcut ctrl-r"
    This reverts commit 8ffd2ceecdebdbd01148dbc8dc6f9089641ad473.

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1223,7 +1223,6 @@
 														}
 
 													// Check if Ctrl + R is pressed
-													<!--
 													if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
 														e.preventDefault();
 														console.log('regenerate');
@@ -1234,7 +1233,6 @@
 
 														regenerateButton?.click();
 													}
-													-->
 
 														if (prompt === '' && e.key == 'ArrowUp') {
 															e.preventDefault();
