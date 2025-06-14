84d08c88fcce29dacff43d08ddb897d42726f3d4
CJ_homepc <changjun.z@gmail.com>
2025-06-14 16:17:04 +0000

    Revert "removed ctrl-r third time"
    This reverts commit fe63a916af16ca006e82271a59a27c7b08e5f546.

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1254,16 +1254,17 @@
 															}
 
 														// Check if Ctrl + R is pressed
-														if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
+														/*
+													if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
 															e.preventDefault();
-															console.log('regenerate skipped');
+															console.log('skipped');
 
 																const regenerateButton = [
 																	...document.getElementsByClassName('regenerate-response-button')
 																]?.at(-1);
 
-															// regenerateButton?.click();
-														}
+														// regenerateButton?.click();
+													}
 
 															if (prompt === '' && e.key == 'ArrowUp') {
 																e.preventDefault();
@@ -1423,7 +1424,7 @@
 														...document.getElementsByClassName('regenerate-response-button')
 													]?.at(-1);
 
-													// regenerateButton?.click();
+													//regenerateButton?.click();
 												}
 
 												if (prompt === '' && e.key == 'ArrowUp') {
