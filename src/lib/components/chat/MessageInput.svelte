e20ff9d22b4e7f4b3b6b3c27cfce1c082787741c
CJ_homepc <changjun.z@gmail.com>
2025-06-14 15:31:51 +0000

    removed shortcut ctrl-r

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -807,6 +807,7 @@
 													}
 
 													// Check if Ctrl + R is pressed
+													<!--
 													if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
 														e.preventDefault();
 														console.log('regenerate');
@@ -817,6 +818,7 @@
 
 														regenerateButton?.click();
 													}
+													-->
 
 													if (prompt === '' && e.key == 'ArrowUp') {
 														e.preventDefault();
