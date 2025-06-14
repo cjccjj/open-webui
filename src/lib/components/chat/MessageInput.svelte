9b8e8c8a00a5046240a5b1ffce1db599e863f287
CJ_homepc <changjun.z@gmail.com>
2025-06-14 15:57:17 +0000

    removed ctrl-r third time

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1142,18 +1142,16 @@
 													}
 
 													// Check if Ctrl + R is pressed
-													/*
 													if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
 														e.preventDefault();
-														console.log('regenerate');
+														console.log('regenerate skipped');
 
 														const regenerateButton = [
 															...document.getElementsByClassName('regenerate-response-button')
 														]?.at(-1);
 
-														regenerateButton?.click();
+														// regenerateButton?.click();
 													}
-													*/
 
 													if (prompt === '' && e.key == 'ArrowUp') {
 														e.preventDefault();
@@ -1336,13 +1334,13 @@
 												// Check if Ctrl + R is pressed
 												if (prompt === '' && isCtrlPressed && e.key.toLowerCase() === 'r') {
 													e.preventDefault();
-													console.log('regenerate');
+													console.log('regenerate skipped');
 
 													const regenerateButton = [
 														...document.getElementsByClassName('regenerate-response-button')
 													]?.at(-1);
 
-													regenerateButton?.click();
+													// regenerateButton?.click();
 												}
 
 												if (prompt === '' && e.key == 'ArrowUp') {
