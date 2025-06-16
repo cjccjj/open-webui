a54c5ec02a80af12f145b3ac5e5db7ab4db90ea2
CJ_homepc <changjun.z@gmail.com>
2025-06-16 13:01:31 +0000

    proper call of lineWrapping

--- src/lib/components/common/CodeEditor.svelte
+++ src/lib/components/common/CodeEditor.svelte
@@ -223,7 +223,7 @@ print("${endTag}")
 
 	let extensions = [
 		basicSetup,
-		lineWrapping,
+		lineWrapping(),
 		keymap.of([{ key: 'Tab', run: acceptCompletion }, indentWithTab]),
 		indentUnit.of('    '),
 		placeholder($i18n.t('Enter your code here...')),
