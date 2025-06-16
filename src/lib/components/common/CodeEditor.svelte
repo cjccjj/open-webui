39a5143b843e134be9c2043fbabeb05bef11109c
CJ_homepc <changjun.z@gmail.com>
2025-06-16 13:20:21 +0000

    proper lineWrapping on 6.0.1

--- src/lib/components/common/CodeEditor.svelte
+++ src/lib/components/common/CodeEditor.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { basicSetup, EditorView } from 'codemirror';
-	import { keymap, placeholder, lineWrapping } from '@codemirror/view';
+	import { keymap, placeholder } from '@codemirror/view';
 	import { Compartment, EditorState } from '@codemirror/state';
 
 	import { acceptCompletion } from '@codemirror/autocomplete';
@@ -212,7 +212,7 @@ print(black.format_str("""${code.replace(/\\/g, '\\\\').replace(/`/g, '\\`').rep
 
 	let extensions = [
 		basicSetup,
-		lineWrapping(),
+		EditorView.lineWrapping,
 		keymap.of([{ key: 'Tab', run: acceptCompletion }, indentWithTab]),
 		indentUnit.of('    '),
 		placeholder('Enter your code here...'),
