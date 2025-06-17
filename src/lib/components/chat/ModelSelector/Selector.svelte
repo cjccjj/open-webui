e7c9e9425a45d04bfdcbc38322e43a762564b3da
CJ_homepc <changjun.z@gmail.com>
2025-06-17 04:05:15 +0000

    Keep the tag ALL

--- src/lib/components/chat/ModelSelector/Selector.svelte
+++ src/lib/components/chat/ModelSelector/Selector.svelte
@@ -438,7 +438,6 @@
 							class="flex gap-1 w-fit text-center text-sm rounded-full bg-transparent px-1.5"
 							bind:this={tagsContainerElement}
 						>
-							<!--
 							{#if items.find((item) => item.model?.connection_type === 'local') || items.find((item) => item.model?.connection_type === 'external') || items.find((item) => item.model?.direct) || tags.length > 0}
 								<button
 									class="min-w-fit outline-none px-1.5 py-0.5 {selectedTag === '' &&
@@ -455,6 +454,7 @@
 								</button>
 							{/if}
 
+							<!--
 							{#if items.find((item) => item.model?.connection_type === 'local')}
 								<button
 									class="min-w-fit outline-none px-1.5 py-0.5 {selectedConnectionType === 'local'
