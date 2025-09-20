7fa03bb245be56b25954899288bacd6482c48aab
CJ at ali-bj <changjun.z@gmail.com>
2025-09-20 14:06:34 +0800

    skip ctrl r

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1264,7 +1264,7 @@
 																	...document.getElementsByClassName('regenerate-response-button')
 																]?.at(-1);
 
-																regenerateButton?.click();
+																//regenerateButton?.click();
 															}
 
 															if (prompt === '' && e.key == 'ArrowUp') {
