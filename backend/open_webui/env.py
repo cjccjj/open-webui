72f74d26f68385f92a86e192b8be727a02d1ec35
CJ at ali-bj <changjun.z@gmail.com>
2025-10-19 15:15:57 +0800

    unlock webui_name change

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -89,7 +89,7 @@ SRC_LOG_LEVELS = {}  # Legacy variable, do not remove
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
 if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+    pass # WEBUI_NAME += " (Open WebUI)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
